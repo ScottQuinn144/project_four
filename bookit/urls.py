80ca6d15a06a2115a185dd1f8e378362538d6eb7
ScottQuinn144 <scott.quinn144@outlook.com>
2022-07-05 12:57:12 +0000

    added views and urls to link all hmtl files in nav bar

--- bookit/urls.py
+++ bookit/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='home'),
+    path('book/', views.book, name='book'),
+    path('contact/', views.contact, name='contact'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('menu/', views.menu, name='menu')
+
 ]
--- bookit/views.py
+++ bookit/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def book(request):
+    return render(request, 'book.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+
+def menu(request):
+    return render(request, 'menu.html')
