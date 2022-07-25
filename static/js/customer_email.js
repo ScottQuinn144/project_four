7a70557a2e12b62c138de0b2d8a678f939d4bb86
ScottQuinn144 <scott.quinn144@outlook.com>
2022-07-25 22:02:48 +0000

    completed email in contact.html

--- static/js/customer_email.js
+++ static/js/customer_email.js
@@ -1,1 +1,20 @@
-console.log('Hello');
+function sendMail(contactForm) {
+    emailjs.send("service_1merlqf","template_ekl3vdo", {
+        "from_name": contactForm.name.value,
+        "message": contactForm.query.value,
+        "from_email": contactForm.emailaddress.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+            window.location.href ='../'
+
+        },
+        function(error) {
+            console.log("FAILED", error);
+            window.location = 'contact.html';
+        }
+    );
+    return false; 
+}
+
