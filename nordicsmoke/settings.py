3246a44696702f3eebdc712fdeb39380747b55e2
ScottQuinn144 <scott.quinn144@outlook.com>
2022-07-27 20:10:42 +0000

    deployment

--- nordicsmoke/settings.py
+++ nordicsmoke/settings.py
@@ -14,11 +14,10 @@ import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import dj_database_url
-
 if os.path.isfile("env.py"):
     import env
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -30,12 +29,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dz4k@cxv#01ss7^)(ws3^fyf!7_m^&$i)er94evihro_#0#&zb'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["nordicsmokehouse.herokuapp.com", "localhost"]
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
